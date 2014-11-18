703c22239cb9b900a419de0a0663deea7550cded
jumpcn <572519236@qq.com>
2014-11-18 20:51:21 +0800

    hehe

--- CodeForces/489A.cpp
+++ CodeForces/489A.cpp
@@ -6,11 +6,33 @@ int d[N];
 int main() {
     int n;
     cin >> n;
-    int* k = new int[n];
-    for(int i = 0; i < n; i++) {
+
+    for(int i = 1; i <= n; i++) {
         cin >> d[i];
-        k[i] = d[i];
     }
 
-    sort(d, d + n);
+    vector<pair<int, int>> ans;
+
+    d[0] = ~0U >> 1;
+
+    for(int i = 1; i < n; i++) {
+        int idx = 0, val = 0;
+        val = d[i];
+        for(int j = i + 1; j <= n; j++) {
+            if(d[j] < val) {
+                val = d[j];
+                idx = j;
+            }
+        }
+        if(idx != 0) {
+            swap(d[i], d[idx]);
+            ans.push_back(make_pair(i, idx));
+        }
+    }
+
+    cout << ans.size() << endl;
+
+    for(int i = 0; i < ans.size(); i++) {
+        cout << ans[i].first << " " << ans[i].second << endl;
+    }
 }
